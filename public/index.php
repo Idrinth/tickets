ad62250302840c47b705b4633b1f3119f44743f1
Björn Büttner <idrinth@users.noreply.github.com>
2023-02-19 01:40:49 +0100

    fix db provide example env

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ use Twig\Loader\FilesystemLoader;
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Application())
-    ->register(new PDO($dsn, $username, $passwd))
+    ->register(new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_DATABASE'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']))
     ->register(new Environment(new FilesystemLoader(dirname(__DIR__) . '/templates')))
     ->get('/', Home::class)
     ->post('/', Home::class)
