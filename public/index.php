f7817f45b765ff4e6a9673dd02844f090cd88e20
Björn Büttner <idrinth@users.noreply.github.com>
2023-02-19 02:00:27 +0100

    fix route order

--- public/index.php
+++ public/index.php
@@ -16,10 +16,10 @@ require dirname(__DIR__) . '/vendor/autoload.php';
     ->register(new Environment(new FilesystemLoader(dirname(__DIR__) . '/templates')))
     ->get('/', Home::class)
     ->post('/', Home::class)
+    ->get('/imprint', Imprint::class)
     ->get('/login', Login::class)
     ->get('/{project:[a-z-]+}', Project::class)
     ->post('/{project:[a-z-]+}', Project::class)
     ->get('/{project:[a-z-]+}/{ticket:[a-zA-Z0-9]+}', Ticket::class)
     ->post('/{project:[a-z-]+}/{ticket:[a-zA-Z0-9]+}', Ticket::class)
-    ->get('/imprint', Imprint::class)
     ->run();
