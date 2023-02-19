ae08711060e163294c539d49d0679f8f8960d8ee
Björn Büttner <idrinth@users.noreply.github.com>
2023-02-19 07:55:28 +0100

    add first ticket view

--- src/Pages/Ticket.php
+++ src/Pages/Ticket.php
@@ -4,8 +4,33 @@ namespace De\Idrinth\Tickets\Pages;
 
 class Ticket
 {
-    public function run()
+    private Twig $twig;
+    private PDO $database;
+
+    public function __construct(Twig $twig, PDO $database)
     {
-        
+        $this->twig = $twig;
+        $this->database = $database;
+    }
+    public function run($post, $category, $ticket)
+    {
+        $stmt = $this->database->prepare('SELECT * FROM projects WHERE slug=:slug');
+        $stmt->execute([':slug' => $category]);
+        $project = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$project) {
+            header('Location: /', true, 303);
+            return;
+        }
+        $stmt = $this->database->prepare('SELECT * FROM tickets WHERE slug=:slug');
+        $stmt->execute([':slug' => $ticket]);
+        $ticket = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$project) {
+            header('Location: /' . $category, true, 303);
+            return;
+        }
+        $stmt = $this->database->prepare('SELECT * FROM comments WHERE ticket=:id');
+        $stmt->execute([':id' => $ticket]);
+        $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this->twig->render('ticket', ['project' => $project, 'ticket' => $ticket, 'comments' => $comments]);
     }
 }
