3d55038a1918d77521e27721e4c6bde64eb4c00d
Björn Büttner <idrinth@users.noreply.github.com>
2023-02-19 14:47:35 +0100

    fix id not set

--- src/Twig.php
+++ src/Twig.php
@@ -20,12 +20,15 @@ class Twig
         $context['projects'] = $this->database
             ->query("SELECT * FROM projects")
             ->fetchAll(PDO::FETCH_ASSOC);
-        $stmt = $this->database
-            ->prepare("SELECT * FROM users WHERE aid=:aid");
-        $stmt->execute([
-            ':aid' => $_SESSION['id'],
-        ]);
-        $context['user'] = $stmt->fetch(PDO::FETCH_ASSOC);
+        $context['user'] = [];
+        if (isset($_SESSION['id'])) {
+            $stmt = $this->database
+                ->prepare("SELECT * FROM users WHERE aid=:aid");
+            $stmt->execute([
+                ':aid' => $_SESSION['id'],
+            ]);
+            $context['user'] = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
         return $this->twig->render("$template.twig", $context);
     }
 }
