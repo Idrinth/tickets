edbb62ae514110ebbc6d9de2183ed6c999de7e38
Björn Büttner <idrinth@users.noreply.github.com>
2023-02-23 16:46:06 +0100

    add changing of data

--- src/Pages/Profile.php
+++ src/Pages/Profile.php
@@ -21,6 +21,11 @@ class Profile
             header('Location: /login', true, 303);
             return;
         }
+        if (isset($post['display']) && isset($post['enable_mail_update']) && isset($post['enable_discord_update'])) {
+            $this->database
+                ->prepare('UPDATE `users` SET `display`=:display,`enable_mail_update`=:enable_mail_update,`enable_discord_update`:enable_discord_update WHERE aid=:id')
+                ->execute([':id' => $_SESSION['id'], ':display' => $post['display'], ':enable_mail_update' => $post['enable_mail_update'], ':enable_discord_update' => $post['enable_discord_update']]);
+        }
         $stmt = $this->database->prepare('SELECT * FROM users WHERE aid=:aid');
         $stmt->execute([':aid' => $_SESSION['id']]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
